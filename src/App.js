b2db88bb0c337120a00cdb8a83682188012df143
Mike Hake <mike.hake@live.com>
2020-08-22 12:45:00 -0600

    Fade in/out status

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 
 function App() {
   const text1 = 'Some text to copy';
+  const text2 = 'Some different text to copy';
   return (
     <div className="App">
       <h1>Copy Widget Demo</h1>
       <span>{text1}</span>
       <CopyWidget text={text1}/>
+      <br/>
+      <span>{text2}</span>
+      <CopyWidget text={text2}/>
+
     </div>
   );
 }
--- src/components/CopyWidget/CopyWidget.jsx
+++ src/components/CopyWidget/CopyWidget.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import icon from './icon_copy.svg';
 import styled from 'styled-components';
 import {CopyToClipboard} from "react-copy-to-clipboard";
-
 
 const Content = styled.div`
   position: relative; 
@@ -46,17 +45,30 @@ const StyledStatus = styled.span`
   height: 20px;
   width: 60px;
   line-height: 20px;
+  transition: opacity 0.5s;
+  opacity: ${({ visible }) => (visible ? '1' : '0')};
 `;
 
+
+
 const CopyWidget = ({text}) => {
+  const [statusVisible, setStatusVisible] = useState(false);
+
+  const handleCopyCick = () => {
+    console.log('Copied:', text)
+    setStatusVisible(true);
+    setTimeout(() => setStatusVisible(false), 3000)
+  };
+
+
   return (
     <Content>
-      <CopyToClipboard text={text} onCopy={() => console.log('Copied:', text)}>
+      <CopyToClipboard text={text} onCopy={handleCopyCick}>
         <StyledButton>
           <img src={icon} alt="copy" className="icon"/>
         </StyledButton> 
       </CopyToClipboard>  
-      <StyledStatus>Copied</StyledStatus>
+      <StyledStatus visible={statusVisible}>Copied</StyledStatus>
     </Content>
   );
 }
